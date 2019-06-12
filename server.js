3d38065f0e1460fd9c9d7e3b091ca1d04b9b7f9d
gitesh <writemail2gitesh@gmail.com>
2019-06-12 23:28:33 +0530

    commit added

--- server.js
+++ server.js
@@ -1,58 +1,53 @@
-const express=require('express')
-const logger=require('morgan')
-const errorhandler=require('errorhandler')
-const mongodb=require('mongodb')
-const bodyParser=require('body-parser')
+const express = require('express')
+const logger = require('morgan')
+const errorhandler = require('errorhandler')
+const mongodb = require('mongodb')
+const bodyParser = require('body-parser')
 
-const url='mongodb://localhost:27017/edx-course-db'
+const url = 'mongodb://localhost:27017/edx-course-db'
 
-let app=express()
+let app = express()
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
-mongodb.MongoClient.connect(url,(error,db)=>{
-    if(error) return process.exit(1)
-    console.log(db)
+mongodb.MongoClient.connect(url, (error, db) => {
+    if (error) return process.exit(1)
 
-    app.get('/accounts',(req,res,next)=>
-    {
+    app.get('/accounts', (req, res, next) => {
         db.collection('accounts')
-        .find({},{sort:{_id: -1}})
-        .toArray((error,accounts)=>{
-            if(error) return next(error)
+            .find({}, { sort: { _id: -1 } })
+            .toArray((error, accounts) => {
+                if (error) return next(error)
+            })
+
+    })
+
+    app.post('/accounts', (req, res, next) => {
+        let newAccount = req.body
+        db.collection('accounts').insert(newAccount, (error, results) => {
+            if (error) return next(error)
+            res.send(results)
         })
 
     })
 
-    app.post('/accounts',(req,res,next)=>
-    {
-             let newAccount=req.body
-             db.collection('accounts').insert(newAccount,(error,results)=>{
-                 if(error) return next(error)
-                 res.send(results)
-             })
-
-    })
-
-    app.put('/accounts/:id',(req,res)=>
-    {
+    app.put('/accounts/:id', (req, res) => {
         db.collection('accounts')
-          .update({_id: mongodb.ObjectId(req.params.id)},{$set: req.body},(error,result)=>{
+            .update({ _id: mongodb.ObjectId(req.params.id) }, { $set: req.body }, (error, result) => {
 
-            if(error)  return next(error)
-            res.send(results)
-     })
-          })
-          
-
-    app.delete('/accounts/:id',(req,res)=>
-    {
-        db.collection('accounts').remove({_id:mongodb.ObjectID(req.param)},(error,result)=>{
-          if(error) return next(error)
-          res.send(results)
+                if (error) return next(error)
+                res.send(results)
+            })
     })
-})
+
+
+    app.delete('/accounts/:id', (req, res) => {
+        db.collection('accounts').remove({ _id: mongodb.ObjectID(req.param) }, (error, result) => {
+            if (error) return next(error)
+            res.send(results)
+        })
+    })
 
     app.use(errorhandler())
     app.listen(3000)
