3f52d24adb50f7e914d28059e0be5ce2f2e1f7fd
varnika <varnika.s20@gmail.com>
2019-06-13 02:42:28 -0700

    update mongo

--- server.js
+++ server.js
@@ -4,21 +4,39 @@ const errorhandler = require('errorhandler')
 const mongodb = require('mongodb')
 const bodyParser = require('body-parser')
 
-const url = 'mongodb://localhost:27017/edx-course-db'
+
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+const url = 'mongodb://localhost:27017/myproject';
+
+const dbName = 'myproject';
+const client = new MongoClient(url);
 
 let app = express()
 
 app.use(logger('dev'))
 app.use(bodyParser.json())
 
-mongodb.MongoClient.connect(url, (error, db) => {
-    if (error) return process.exit(1)
+
+client.connect(url,(error,db)=> {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+  
+    const db = client.db(dbName);
+  
+    
+  
+
+//mongodb.MongoClient.connect(url, (error, db) => {
+  //  if (error) return process.exit(1)
 
     app.get('/accounts', (req, res, next) => {
         db.collection('accounts')
             .find({}, { sort: { _id: -1 } })
             .toArray((error, accounts) => {
                 if (error) return next(error)
+                client.close();
             })
 
     })
@@ -28,6 +46,7 @@ mongodb.MongoClient.connect(url, (error, db) => {
         db.collection('accounts').insert(newAccount, (error, results) => {
             if (error) return next(error)
             res.send(results)
+            client.close();
         })
 
     })
@@ -43,12 +62,13 @@ mongodb.MongoClient.connect(url, (error, db) => {
 
 
     app.delete('/accounts/:id', (req, res) => {
-        db.collection('accounts').remove({ _id: mongodb.ObjectID(req.param) }, (error, result) => {
+        db.collection('accounts').remove({ _id: mongodb.ObjectID(req.param.id) }, (error, result) => {
             if (error) return next(error)
             res.send(results)
         })
     })
-
+    console.log(db)
+    
     app.use(errorhandler())
     app.listen(3000)
 })
